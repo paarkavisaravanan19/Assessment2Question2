e3eb1bc4e9c1f11e0b9d94c8d64261c4f4b85b91
Paarkavi Priya S <paarkavi19092001@gmail.com>
2023-03-31 16:48:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Week2AssessmentQuestion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+            List<string> list = new List<string>() { "Paarkavi", "Priya", "Vartha"}; 
+            License(name, rnd.Next(3), list);
+            
+        }
+        public static void License(string name, int available_number, List<string> list)
+        {
+            list.Add(name);
+            list.Sort();
+            int index = list.FindIndex(a => a.Contains(name));
+            if(index == 0)
+            {
+                Console.WriteLine(20);
+            }
+            if (index == 1)
+            {
+                Console.WriteLine(40);
+            }
+            if (index == 2)
+            {
+                Console.WriteLine(60);
+            }
+            if (index == 3)
+            {
+                Console.WriteLine(80);
+            }
+            if (index == 4)
+            {
+                Console.WriteLine(100);
+            }
+            
+
+
+        }
+
+    }
+}
