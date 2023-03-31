8da72b01b83b268a5a4b03b3f9c451ecb91e2c94
Paarkavi Priya S <68461595+paarkavisaravanan19@users.noreply.github.com>
2023-03-31 17:29:56 +0530

    Add files via upload

---
+++ LicenseWaitingTimeTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Week2AssessmentQuestion2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2AssessmentQuestion2.Tests
+{
+    [TestClass()]
+    public class LicenseWaitingTimeTests
+    {
+        [TestMethod()]
+        public void LicenseTest()
+        {
+            Random rnd = new Random();
+            LicenseWaitingTime wt = new LicenseWaitingTime();
+            List<string> list = new List<string>() { "Paarkavi", "Priya", "Vartha" };
+            wt.License("Dwaraka", rnd.Next(3), list);
+            int Expected = 60; //20==> correct answer
+            Assert.AreEqual(Expected, wt.ResultantIndex);
+            
+        }
+    }
+}
